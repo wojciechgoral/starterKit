725d55db88c406a58580f35e90686fb621a40fe8
Wojciech Góral <wojciech.goral@capgemini.com>
2023-01-10 09:48:59 +0000

    adding fragment

--- webapp/controller/CustomerDetails.controller.js
+++ webapp/controller/CustomerDetails.controller.js
@@ -1,13 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "stk/starterkit/model/formatter",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Formatter, Fragment) {
         "use strict";
+        
 
         return Controller.extend("stk.starterkit.controller.CustomerDetails", {
+            formatter: Formatter,
             onInit: function () {
                 const oRouter = sap.ui.core.UIComponent.getRouterFor(this)
                 oRouter.getRoute("CustomerDetails").attachPatternMatched(this._onPatternMatched, this)
@@ -16,11 +20,40 @@ sap.ui.define([
         _onPatternMatched: function(oEvent) {
                 this.getView().bindElement({
                     path: "/Customers('" + oEvent.getParameter("arguments").CustomerID + "')",
-    
                 })
+            },
+
+        
+
+            onPress: function () {
+
+                var oView = this.getView();
+    
+                if (!this.byId("contactDialog")) {
+    
+                    this.loadFragment({
+    
+                        name: "stk.starterkit.view.ContactInfoDialog"
+    
+                    }).then(function (oDialog) {
+    
+                        oView.addDependent(oDialog);
+    
+                        oDialog.open();
+    
+                    });
+    
+                } else {
+    
+                    this.byId("contactDialog").open();
+    
+                }},
+            onCloseDialog: function(){
+                this.byId("contactDialog").close()
             }
         });
 
+        
         
     });
 
